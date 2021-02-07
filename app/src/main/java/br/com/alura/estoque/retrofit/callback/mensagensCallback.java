13bb24ac3a6fa2c42b9cb9db77b3fbf1fc024341
Pedro Lucas <096823@aluno.uricer.edu.br>
2021-02-07 18:31:34 -0300

    primeiro commit

---
+++ app/src/main/java/br/com/alura/estoque/retrofit/callback/mensagensCallback.java
@@ -0,0 +1,7 @@
+package br.com.alura.estoque.retrofit.callback;
+
+interface mensagensCallback {
+
+    String MENSAGEM_ERRO_RESPOSTA_NAO_SUCEDIDA = "Resposta não secedida";
+    String MENSAGEM_ERRO_FALHA_COMUNICACAO = "Falha de comunicação: ";
+}
